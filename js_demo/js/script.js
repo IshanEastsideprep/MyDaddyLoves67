2c248b2ddb11ca0eec4f8ee58ee0ba9ceab55ad5
IshanEastsideprep <isarvaiya@eastsideprep.org>
2025-11-06 15:01:33 -0800

    Add files via upload

---
+++ js_demo/js/script.js
@@ -0,0 +1,8 @@
+function askQueshtion() {
+    var p = prompt('How much would a woodchuck chuck if a woodchuck could chuck wood');
+    if (p !=null){
+        document.getElementById
+        ('queshtion').innerHTML = 
+        'YIKES! ' + p + ' is a lot of wood!';
+    }
+}
